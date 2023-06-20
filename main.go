68cff503636056ae490d362647885a6778db6cfb
nomoruyi <nomoruyi@stud.hs-heilbronn.de>
2023-06-20 17:10:53 +0200

    save

--- main.go
+++ main.go
@@ -1,24 +1,8 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
 )
-
-var (
-	personTpl     = template.Must(template.ParseFiles("person.gohtml"))
-	changeNameTpl = template.Must(template.ParseFiles("change-name.gohtml"))
-)
-
-type person struct {
-	Name string
-	Age  int
-}
-
-var daniel = person{
-	Name: "Daniel",
-	Age:  22,
-}
 
 func main() {
 	mux := http.NewServeMux()
@@ -31,22 +15,4 @@ func main() {
 	}
 
 	srv.ListenAndServe()
-}
-
-func personHandler(w http.ResponseWriter, r *http.Request) {
-	personTpl.Execute(w, daniel)
-}
-
-func changeNameHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		changeNameTpl.Execute(w, daniel)
-		return
-	}
-
-	r.ParseForm()
-
-	newName := r.Form["name"][0]
-	daniel.Name = newName
-
-	http.Redirect(w, r, "/", http.StatusFound)
 }
